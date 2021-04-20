c8535373fef89404fd1c19f8883e07adcdc6c23f
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-04-20 13:13:26 -0300

    adicionado o json server e a chamada de página estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <h1></h1>
   );
+}
+
+export async function getStaticProps() {
+
+  const response = await fetch('http://localhost:3333/episodes')
+
+  const data = response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
